d9555dc23ebe6775f2745c2c11c9f46e5aa072af
Judah Oyedele <oyedeleinioluwajudah@gmail.com>
2023-12-23 12:10:20 +0100

    changes services styles

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,6 +1,6 @@
 function ServiceCard({ image, title, text }) {
     return (
-        <div className="card col-md-4">
+        <div className="card col-md-5">
             <img src={image} alt={title} />
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
